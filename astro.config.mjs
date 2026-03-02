8f5bfb407b183b25758c789a900087f9af9ebc53
safeer <asafeer1994@gmail.com>
2026-03-02 23:37:40 +0500

    fix: switch to static output, add LMS pages to dist, fix duplicate code in courses/index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
   server: {
     port: 3000
   },
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()]
